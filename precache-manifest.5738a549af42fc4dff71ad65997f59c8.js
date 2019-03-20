43051afd732e1119ae2158f45d29cfa947456de2
Harsh Makani <harshvardhanmakani@gmail.com>
2019-03-19 23:17:05 -0500

    Updates

---
+++ precache-manifest.5738a549af42fc4dff71ad65997f59c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84861f804f672b1a3a6b",
+    "url": "/test-react-todomvc/static/js/runtime~main.3c36be03.js"
+  },
+  {
+    "revision": "82023a59ca5f238bfc8c",
+    "url": "/test-react-todomvc/static/js/main.8b24ed9d.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/test-react-todomvc/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "82023a59ca5f238bfc8c",
+    "url": "/test-react-todomvc/static/css/main.bbfa46cb.chunk.css"
+  },
+  {
+    "revision": "d9616cb52619ca12143326500b098fd3",
+    "url": "/test-react-todomvc/index.html"
+  }
+];
